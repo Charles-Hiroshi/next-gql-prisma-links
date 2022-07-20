3ac988eaee68f39ad110ea368f425d8ea684d192
Charles Hiroshi <charles_hiroshi@outlook.com.br>
2022-07-20 23:05:20 +0900

    final code

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,15 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import GqlCliente from '../graphql/apollo-client'
+import { gql } from '@apollo/client'
+import { Link } from '../types/Link'
 
-const Home: NextPage = () => {
+type Props = {
+  links: Link[]
+}
+
+const Home = ({ links }: Props) => {
   return (
     <div className={styles.container}>
       <Head>
@@ -13,43 +20,17 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <h1 className={styles.title}>Lista de Links</h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
+        <p className={styles.description}>Total de Links: {links.length}</p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          {links.map((link, index) => (
+            <a key={index} href={link.url} className={styles.card}>
+              <h2>{link.title} &rarr;</h2>
+              <p>{link.url}</p>
+            </a>
+          ))}
         </div>
       </main>
 
@@ -70,3 +51,22 @@ const Home: NextPage = () => {
 }
 
 export default Home
+
+export const getServerSideProps = async () => {
+  const { data } = await GqlCliente.query({
+    query: gql`
+      query {
+        links {
+          id
+          title
+          url
+        }
+      }
+    `
+  })
+  return {
+    props: {
+      links: data.links
+    }
+  }
+}
